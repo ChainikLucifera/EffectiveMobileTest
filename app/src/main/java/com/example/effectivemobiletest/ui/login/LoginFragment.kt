8c4777e99da10c3150485265d34f20061f100828
Artyom <goglakk2@gmail.com>
2025-11-18 18:26:30 +0200

    Login page, no icons

---
+++ app/src/main/java/com/example/effectivemobiletest/ui/login/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.example.effectivemobiletest.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.effectivemobiletest.R
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = LoginFragment()
+    }
+}
---
+++ app/src/main/java/com/example/effectivemobiletest/ui/login/LoginViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.effectivemobiletest.ui.login
+
+class LoginViewModel {
+}
